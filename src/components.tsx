91b3fa10c0f6be87e45c3df773ee16fd1a328996
andrewgeraci <79218071+andrewgeraci@users.noreply.github.com>
2022-10-20 17:52:37 -0400

    counter

--- src/components.tsx
+++ src/components.tsx
@@ -6,7 +6,7 @@ export function AddSubtract(): JSX.Element {
     return (
         <span>
             <Button onClick={() => setValue(1 + value)}>Add One</Button>
-            to {value}.{"  "}
+            {" "}to {value}.{"  "}
             <Button onClick={() => setValue(value - 1)}>Subtract One</Button>
         from {value}.
         </span>
@@ -17,8 +17,8 @@ export function Counter(): JSX.Element {
     const [value, setValue] = useState<number>(0);
     return (
         <span>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
-            to {value}.
+            <Button onClick={() => setValue(1 + value)}>This Counter Will Add One</Button>
+            {" "}to {value}.
         </span>
     );
 }
