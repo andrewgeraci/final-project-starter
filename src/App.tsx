6ffc61d0939149fa88ed8b0ffa30358b075a5f7d
andrewgeraci <79218071+andrewgeraci@users.noreply.github.com>
2022-10-20 17:43:27 -0400

    added counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {AndrewCounter} from "./components";
+import {AddSubtract, Counter} from "./components";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                Welcome to UD CIS Scheduler!
+                Welcome!
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
@@ -16,8 +16,9 @@ function App(): JSX.Element {
             <div>
                 {/* I figured we could each do our own counter since we need multiple custom labels. 
                 Mine has the subract in it so that one's done */}
-                <AndrewCounter></AndrewCounter>
+                <AddSubtract></AddSubtract>
             </div>
+            <Counter></Counter>
         </div>
     );
 }
